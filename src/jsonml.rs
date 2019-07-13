312311dd56c60108fcea647c91bc5513ce00229b
Yiran Sheng <shengyiran@gmail.com>
2019-07-13 12:38:29 +0800

    Parse markdown events to jsonml

--- src/jsonml.rs
+++ src/jsonml.rs
@@ -1,66 +1,263 @@
-use std::iter::FromIterator;
+use std::iter::{FromIterator, Peekable};
 
-use pulldown_cmark::{CowStr, Event, Parser, Tag};
+use pulldown_cmark::{CowStr, Event, LinkType, Parser, Tag};
 use serde::ser::{Serialize, SerializeSeq, Serializer};
+use serde_derive::*;
 
+#[derive(Debug)]
 pub struct JsonML<'a> {
     elements: Vec<Element<'a>>,
 }
 
+#[derive(Debug)]
 pub enum Element<'a> {
     Text(CowStr<'a>),
     Tree(&'static str, Option<Attrs<'a>>, Vec<Element<'a>>),
     SelfClosing(&'static str, Option<Attrs<'a>>),
 }
 
+#[derive(Debug)]
 pub struct Attrs<'a> {
     attrs: Vec<Attr<'a>>,
 }
 
+#[derive(Debug)]
 struct Attr<'a> {
     key: CowStr<'a>,
     value: CowStr<'a>,
 }
 
-pub struct MDError {}
+#[derive(Debug, Serialize)]
+pub enum ParseError {
+    EOF,
+    ComplexTag,
+    Expects(&'static str),
+    Unexpected(&'static str),
+    NotSupported(&'static str),
+    Custom(&'static str),
+}
 
-// impl<'a> FromIterator<Event<'a>> for JsonML<'a> {
-// fn from_iter<T>(iter: T) -> Self
-// where
-// T: IntoIterator<Item = Event<'a>>,
-// {
+pub struct MDParser<I: Iterator> {
+    events: Peekable<I>,
+}
 
-// }
-// }
+type IResult<T> = Result<T, ParseError>;
 
-// fn text<'a, I>(iter: &mut I) -> Result<Element<'a>, MDError>
-// where I: Iterator<Item = Event<'a>>,
-// {
-// match iter.next() {
-// Some(Event::Text(t)) => Ok(Element::Text(t)),
-// Some(Event::
-// }
+impl<'a> MDParser<Parser<'a>> {
+    pub fn new(raw: &'a str) -> Self {
+        let parser = Parser::new(raw);
+        Self {
+            events: parser.peekable(),
+        }
+    }
+    #[inline(always)]
+    pub fn jsonml(&mut self) -> IResult<JsonML<'a>> {
+        Ok(JsonML {
+            elements: self.elements()?,
+        })
+    }
+}
 
-// }
+impl<'a, I> MDParser<I>
+where
+    I: Iterator<Item = Event<'a>>,
+{
+    fn current(&mut self) -> IResult<&Event<'a>> {
+        dbg!(self.events.peek());
+        self.events.peek().ok_or_else(|| ParseError::EOF)
+    }
+    fn advance(&mut self) -> IResult<Event<'a>> {
+        self.events.next().ok_or_else(|| ParseError::EOF)
+    }
+    fn elements(&mut self) -> IResult<Vec<Element<'a>>> {
+        let mut elements = vec![];
+        while let Ok(el) = self.maybe_element() {
+            if let Some(el) = el {
+                // dbg!(&el);
+                elements.push(el);
+            } else {
+                break;
+            }
+        }
 
-// fn convert_tag<'a>(tag: Tag<'a>) -> &'static str {
-// match tag {
-// Tag::Paragraph => "p",
-// Tag::Rule => "hr",
-// Tag::Header(1) => "h1",
-// Tag::Header(2) => "h2",
-// Tag::Header(3) => "h3",
-// Tag::Header(4) => "h4",
-// Tag::Header(5) => "h5",
-// Tag::Header(_) => "h6",
-// Tag::BlockQuote => "blockquote",
-// Tag::CodeBlock(_) => "code",
-// Tag::List(_) => "ul",
-// Tag::Item => "li",
-// Tag::Emphasis => "em",
-// Tag::Strong => "strong",
-// }
-// }
+        Ok(elements)
+    }
+    fn maybe_element(&mut self) -> IResult<Option<Element<'a>>> {
+        let current = match self.current() {
+            Ok(event) => event,
+            Err(ParseError::EOF) => return Ok(None),
+            Err(_) => unreachable!(),
+        };
+
+        let el = match current {
+            Event::Text(_) => self.text()?,
+            Event::Start(_) => self.tag()?,
+            Event::SoftBreak => Element::Text("\n".into()),
+            Event::HardBreak => Element::SelfClosing("hr", None),
+            Event::Code(_) => unimplemented!(),
+            Event::End(_) => return Ok(None),
+            _ => return Err(ParseError::NotSupported("unsupported feature")),
+        };
+
+        Ok(Some(el))
+    }
+
+    fn text(&mut self) -> IResult<Element<'a>> {
+        match self.advance()? {
+            Event::Text(contents) => Ok(Element::Text(contents)),
+            _ => Err(ParseError::Expects("text")),
+        }
+    }
+    fn tag(&mut self) -> IResult<Element<'a>> {
+        match self.peek_basic_tag() {
+            Ok(tag) => self.basic_tag(tag),
+            Err(ParseError::ComplexTag) => self.complex_tag(),
+            Err(err) => Err(err),
+        }
+    }
+    fn peek_basic_tag(&mut self) -> IResult<&'static str> {
+        match self.current()? {
+            Event::Start(ref tag) => to_basic_tag(tag),
+            _ => Err(ParseError::Expects("tag open")),
+        }
+    }
+    fn complex_tag(&mut self) -> IResult<Element<'a>> {
+        match self.advance()? {
+            Event::Start(tag) => match tag {
+                Tag::Rule => Ok(Element::SelfClosing("hr", None)),
+                Tag::List(Some(1)) => {
+                    let elements = self.elements()?;
+                    let el = Element::Tree("ol", None, elements);
+                    self.expects_close()?;
+                    self.advance()?;
+                    Ok(el)
+                }
+                Tag::List(Some(start)) => {
+                    let elements = self.elements()?;
+                    let attr = Attr {
+                        key: "start".into(),
+                        value: start.to_string().into(),
+                    };
+                    let el = Element::Tree(
+                        "ol",
+                        Some(Attrs { attrs: vec![attr] }),
+                        elements,
+                    );
+                    self.expects_close()?;
+                    self.advance()?;
+                    Ok(el)
+                }
+                Tag::Link(ty, dest, title) => self.link(ty, dest, title),
+                Tag::Image(ty, dest, title) => self.image(ty, dest, title),
+                _ => Err(ParseError::Expects("open tag")),
+            },
+            _ => Err(ParseError::Expects("open tag")),
+        }
+    }
+    fn link(
+        &mut self,
+        ty: LinkType,
+        dest: CowStr<'a>,
+        _title: CowStr<'a>,
+    ) -> IResult<Element<'a>> {
+        match ty {
+            LinkType::Email => return Err(ParseError::NotSupported("mailto")),
+            _ => {}
+        }
+
+        let attrs = Attrs {
+            attrs: vec![Attr {
+                key: "href".into(),
+                value: dest,
+            }],
+        };
+        let elements = self.elements()?;
+        self.expects_close()?;
+        self.advance()?;
+
+        Ok(Element::Tree("a", Some(attrs), elements))
+    }
+    fn image(
+        &mut self,
+        ty: LinkType,
+        dest: CowStr<'a>,
+        title: CowStr<'a>,
+    ) -> IResult<Element<'a>> {
+        match ty {
+            LinkType::Email => return Err(ParseError::NotSupported("mailto")),
+            _ => {}
+        }
+
+        let attrs = Attrs {
+            attrs: vec![
+                Attr {
+                    key: "src".into(),
+                    value: dest,
+                },
+                Attr {
+                    key: "alt".into(),
+                    value: title,
+                },
+            ],
+        };
+
+        Ok(Element::SelfClosing("img", Some(attrs)))
+    }
+
+    fn basic_tag(&mut self, open_tag: &'static str) -> IResult<Element<'a>> {
+        self.advance()?;
+        let elements = self.elements()?;
+        match self.current()? {
+            Event::End(ref tag) => {
+                let close_tag = to_basic_tag(tag)?;
+                if close_tag == open_tag {
+                    self.advance()?;
+                    Ok(Element::Tree(open_tag, None, elements))
+                } else {
+                    Err(ParseError::Unexpected(close_tag))
+                }
+            }
+            _ => Err(ParseError::Expects("tag close")),
+        }
+    }
+    fn expects_close(&mut self) -> IResult<()> {
+        match self.current()? {
+            Event::End(_) => Ok(()),
+            _ => Err(ParseError::Expects("close tag")),
+        }
+    }
+}
+
+fn to_basic_tag(tag: &Tag) -> IResult<&'static str> {
+    let tag = match tag {
+        Tag::Paragraph => "p",
+        // does not support h1, h2
+        Tag::Header(3) => "h3",
+        Tag::Header(4) => "h4",
+        Tag::Header(5) => "h5",
+        Tag::Header(6) => "h6",
+        Tag::BlockQuote => "blockquote",
+        Tag::Strong => "strong",
+        Tag::Item => "li",
+        Tag::List(None) => "ul",
+        Tag::Emphasis => "em",
+        // not supported
+        Tag::Header(_) => return Err(ParseError::NotSupported("h1/h2")),
+        Tag::FootnoteDefinition(_) => {
+            return Err(ParseError::NotSupported("footnote"))
+        }
+        Tag::HtmlBlock => return Err(ParseError::NotSupported("html")),
+        Tag::Table(_) => return Err(ParseError::NotSupported("table")),
+        Tag::TableRow => return Err(ParseError::NotSupported("tr")),
+        Tag::TableCell => return Err(ParseError::NotSupported("tr")),
+        Tag::Strikethrough => {
+            return Err(ParseError::NotSupported("strikethrough"))
+        }
+        _ => return Err(ParseError::ComplexTag),
+    };
+
+    Ok(tag)
+}
 
 impl<'a> Serialize for JsonML<'a> {
     fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
@@ -178,5 +375,25 @@ mod tests {
                 ["input"],
             ])
         );
+    }
+
+    #[test]
+    fn test_from_md_events() {
+        let raw = r#"### Header 3
+This is paragraph, with [Link](https://www.google.com).
+
+* Foo
+* Bar
+* Baz
+- Baz 1
+- Baz 2
+"#;
+
+        let jsonml = MDParser::new(raw).jsonml().unwrap();
+        let serialized = serde_json::to_string_pretty(&jsonml).unwrap();
+
+        eprintln!("Parsed: \n{}", serialized);
+
+        assert!(false);
     }
 }
